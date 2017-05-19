4bf7d39e6ad20d751aec4d094761e72048b6f5e1
Patrique Ouimet <patrique.ouimet@gmail.com>
2017-05-19 16:21:47 -0400

    Adding service provider to bootstrap driver

---
+++ src/TestingScoutServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PatOui\Scout;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Scout\EngineManager;
+
+class TestingScoutServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app[EngineManager::class]->extend('testing', function () {
+            return new Engines\TestingEngine;
+        });
+    }
+}
